d9fec016ba90088eecd20c8eb1e5833b57ea7207
yedongfu <dongfuye@163.com>
2021-07-13 15:43:16 +0800

    sample ok

---
+++ index.js
@@ -0,0 +1,50 @@
+const Koa = require('koa')
+const koaBody = require('koa-body')
+const Router = require('@koa/router')
+const dtmcli = require("dtmcli")
+
+async function FireTcc() {
+  let dtm = "http://localhost:8080/api/dtmsvr"
+  let svc = "http://localhost:4005/api"
+  await dtmcli.tccGlobalTransaction(dtm, async (t) => {
+    let req = { amount: 30 }
+    console.log("calling trans out")
+    await t.callBranch(req, svc + "/TransOutTry", svc + "/TransOutConfirm", svc + "/TransOutCancel")
+    console.log("calling trans in")
+    await t.callBranch(req, svc + "/TransInTry", svc + "/TransInConfirm", svc + "/TransInCancel")
+  })
+}
+
+
+const app = new Koa()
+const router = new Router()
+
+router.post('/api/TransOutTry', (ctx, next) => {
+  console.log("TransOutTry")
+  ctx.body = { result: "SUCCESS" }
+}).post('/api/TransOutConfirm', (ctx, next) => {
+  console.log("TransOutConfirm")
+  ctx.body = { result: "SUCCESS" }
+}).post('/api/TransOutCancel', (ctx, next) => {
+  console.log("TransOutCancel")
+  ctx.body = { result: "SUCCESS" }
+}).post('/api/TransInTry', (ctx, next) => {
+  console.log("TransInTry")
+  ctx.body = { result: "SUCCESS" }
+}).post('/api/TransInConfirm', (ctx, next) => {
+  console.log("TransInConfirm")
+  ctx.body = { result: "SUCCESS" }
+}).post('/api/TransInCancel', (ctx, next) => {
+  console.log("TransInCancel")
+  ctx.body = { result: "SUCCESS" }
+}).get('/api/FireTcc', async (ctx, next) => {
+  await FireTcc()
+  ctx.body = { result: "SUCCESS" }
+})
+
+app.use(koaBody({
+  jsonLimit: '100kb'
+})).use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(4005, FireTcc)
