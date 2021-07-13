a4b04e651df933a0015329332cc78d79d1881557
yedongfu <dongfuye@163.com>
2021-07-13 19:25:26 +0800

    add comment

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const Router = require('@koa/router')
 const dtmcli = require("dtmcli")
 
 async function FireTcc() {
-  let dtm = "http://localhost:8080/api/dtmsvr"
-  let svc = "http://localhost:4005/api"
+  let dtm = "http://localhost:8080/api/dtmsvr" // dtm服务地址
+  let svc = "http://localhost:4005/api" // 本地服务前缀
+  // 开启一个tcc事务，第二个参数里面，写全局事务的逻辑
   await dtmcli.tccGlobalTransaction(dtm, async (t) => {
-    let req = { amount: 30 }
+    let req = { amount: 30 } // 子事务需要的负荷
     console.log("calling trans out")
+    // 注册事务分支，并调用分支中的Try
     await t.callBranch(req, svc + "/TransOutTry", svc + "/TransOutConfirm", svc + "/TransOutCancel")
     console.log("calling trans in")
+    // 注册事务分支，并调用分支中的Try
     await t.callBranch(req, svc + "/TransInTry", svc + "/TransInConfirm", svc + "/TransInCancel")
   })
 }
